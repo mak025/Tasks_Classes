0c9925c252ab2d01ea3fe576d4738a624e963a38
mak025 <mak025@edu.zealand.dk>
2025-03-17 10:40:44 +0100

    Added tasks palindrome and PlasticFantastic

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to checkif its a palindrome");
+            string word = Console.ReadLine();
+            Console.WriteLine(CheckPalindrome(word));
+        }
+        public static bool CheckPalindrome(string word)
+        {
+            Stack<char> chars = new Stack<char>();
+            foreach (char letter in word)
+            {
+                chars.Push(letter);
+            }
+            foreach(char letter in word)
+            {
+                if (letter != chars.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
